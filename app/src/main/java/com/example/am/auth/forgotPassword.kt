a31d22f41b323c9f7db978891dbb7f817595108a
Amos <st10104787@rcconnect.edu.za>
2023-10-16 14:18:45 +0000

    Added forgot password page

---
+++ app/src/main/java/com/example/am/auth/forgotPassword.kt
@@ -0,0 +1,12 @@
+package com.example.am.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.am.R
+
+class forgotPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
