8951d09c4b71f75904ffea4f710b178e0b9f90e8
Amos <st10104787@rcconnect.edu.za>
2023-10-16 13:57:58 +0000

    Added sign up page

---
+++ app/src/main/java/com/example/am/auth/Register.kt
@@ -0,0 +1,12 @@
+package com.example.am.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.am.R
+
+class Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
